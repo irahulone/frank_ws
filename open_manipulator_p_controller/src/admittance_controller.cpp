efc3203b1ace5a324313bfa227a5eaa770191c17
kevin866 <k.wang1502483@gmail.com>
2025-07-03 08:21:38 -0700

    update controller to publish jacobian in ee frame

--- open_manipulator_p_controller/src/admittance_controller.cpp
+++ open_manipulator_p_controller/src/admittance_controller.cpp
@@ -1,18 +1,27 @@
 #include "open_manipulator_p_controller/admittance_controller.h"
-
+#include <ros/ros.h>
 #include <geometry_msgs/Wrench.h>
 #include <Eigen/Dense>
+using namespace robotis_manipulator;
 
 namespace open_manipulator_p_controllers
 {
 
-AdmittanceController::AdmittanceController(ros::NodeHandle& nh) : nh_(nh)
+AdmittanceController::AdmittanceController(ros::NodeHandle& nh, const std::string& usb_port, const std::string& baud_rate)
+: nh_(nh),priv_node_handle_("~"),
+  with_gripper_(false),
+  usb_port_(usb_port),
+  baud_rate_(baud_rate)
 {
   init();
 }
 
+
 bool AdmittanceController::init()
 {
+  control_period_       = priv_node_handle_.param<double>("control_period", 0.010f);
+  using_platform_       = priv_node_handle_.param<bool>("using_platform", false);
+  with_gripper_         = priv_node_handle_.param<bool>("with_gripper", false);
   // Subscriber: joint_states
   joint_state_sub_ = nh_.subscribe("/joint_states", 10, &AdmittanceController::jointStateCb, this);
 
@@ -21,9 +30,13 @@ bool AdmittanceController::init()
 
   ROS_INFO("Admittance controller initialized");
 
+  open_manipulator_.initOpenManipulator(using_platform_, usb_port_, baud_rate_, control_period_, with_gripper_);
+
   // You may need to load or define manipulator kinematics
   // For example, you might set up the kinematic chain here
   // Or load the robot description and call manipulator_.addComponent(), etc.
+  // kinematics::SolverUsingCRAndJacobian kinematics_solver_;
+
 
   return true;
 }
@@ -47,11 +60,12 @@ void AdmittanceController::jointStateCb(const sensor_msgs::JointState::ConstPtr&
     joint_values.push_back(jv);
   }
 
-  manipulator_.setAllJointValue(joint_values);
+  // manipulator_.setAllJointValue(joint_values);
 
   // Step 2: Compute Jacobian at the tool frame
-  Eigen::MatrixXd J = kinematics_solver_.jacobian(&manipulator_, "tool");  // use your tool name here
+  Eigen::MatrixXd J = open_manipulator_.jacobian("gripper");
 
+  ROS_INFO_STREAM("Some message: " << J);
   // Step 3: Get joint effort as Eigen vector
   Eigen::VectorXd tau(msg->effort.size());
   for (size_t i = 0; i < msg->effort.size(); ++i)
@@ -84,8 +98,36 @@ void AdmittanceController::jointStateCb(const sensor_msgs::JointState::ConstPtr&
 
 Eigen::VectorXd AdmittanceController::computeEndEffectorWrench(const Eigen::VectorXd& joint_effort)
 {
-  Eigen::MatrixXd J = kinematics_solver_.jacobian(&manipulator_, "tool");
+  Eigen::MatrixXd J = open_manipulator_.jacobian("gripper");
   return J * joint_effort;
 }
 
 } // namespace open_manipulator_p_controllers
+
+
+int main(int argc, char **argv)
+{
+  // init
+  ros::init(argc, argv, "admittance_controller");
+  // ros::NodeHandle node_handle("");
+  ros::NodeHandle nh;
+
+  std::string usb_port = "/dev/ttyUSB0";
+  std::string baud_rate = "1000000";
+
+  if (argc == 3)
+  {
+    usb_port = argv[1];
+    baud_rate = argv[2];
+    printf("port_name and baud_rate are set to %s, %s \n", usb_port.c_str(), baud_rate.c_str());
+  }
+  else
+  {
+    log::error("Please set '-port_name' and  '-baud_rate' arguments for connected Dynamixels");
+    return 1;
+  }
+  open_manipulator_p_controllers::AdmittanceController controller(nh, usb_port, baud_rate);
+
+  ros::spin();
+  return 0;
+}
